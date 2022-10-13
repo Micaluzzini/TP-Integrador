261b63bd2d7fc2be66a6005efee45ff5d92357ca
Micaluzzini <micaweilenmann@gmail.com>
2022-10-13 19:15:49 -0300

    auto
    cambios en auto

--- auto.py
+++ auto.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 
+import datetime
+
 
 class Auto:
-      def __init__(self, patente, modelo, marca, nombre, apellido, dni, cant_puertas):
+      def __init__(self, tipo, patente, modelo, marca, nombre, apellido, dni, cant_puertas_traccion):
+        self.tipo = tipo
         self.patente = patente
         self.modelo = modelo
         self.marca = marca
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
-        self.cant_puertas = cant_puertas
+        self.cant_puertas_traccion = cant_puertas_traccion
+        self.fecha_creacion = datetime.date.today()
         
       def mostrar_datos(self):
+        texto = f"Tipo: {self.tipo}\n"
         texto = f"Patente y modelo: {self.patente} {self.modelo}\n"
         texto += f"Marca: {self.marca}\n"
         texto = f"Nombre y apellido: {self.nombre} {self.apellido}\n"
         texto += f"DNI: {self.dni}\n"
-        texto += f"Cantidad de puertas: {self.cant_puertas}\n"
+        texto += f"Cantidad de puertas: {self.cant_puertas_traccion}\n"
         
         return texto
       
-      def coincide(self, patente_a_buscar):
-        if patente_a_buscar in self.patente:
+      def coincide(self, texto_a_buscar):
+        if texto_a_buscar.upper() in self.nombre.upper() or texto_a_buscar.upper() in self.apellido.upper():
             return True
         else:
             return False
