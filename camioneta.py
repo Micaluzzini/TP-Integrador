348935efc8c95145059e4904fa47595e299988a0
Micaluzzini <micaweilenmann@gmail.com>
2022-10-13 19:21:19 -0300

    camioneta
    cambios en camioneta. agregado de datetime

--- camioneta.py
+++ camioneta.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 
+import datetime
+
 
 class Camioneta:
-      def __init__(self, patente, modelo, marca, nombre, apellido, dni, tipo_traccion):
+      def __init__(self, tipo, patente, modelo, marca, nombre, apellido, dni, cant_puertas_traccion):
+        self.tipo = tipo
         self.patente = patente
         self.modelo = modelo
         self.marca = marca
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
-        self.tipo_traccion = tipo_traccion
+        self.cant_puertas_traccion = cant_puertas_traccion
+        self.fecha_creacion = datetime.date.today()
         
         
       def mostrar_datos(self):
+        texto = f"Tipo: {self.tipo}\n"
         texto = f"Patente y modelo: {self.patente} {self.modelo}\n"
         texto += f"Marca: {self.marca}\n"
         texto = f"Nombre y apellido: {self.nombre} {self.apellido}\n"
         texto += f"DNI: {self.dni}\n"
-        texto += f"tipo_traccion: {self.tipo_traccion}\n"
+        texto += f"tipo_traccion: {self.cant_puertas_traccion}\n"
         
         return texto  
       
-      def buscar_por_patente(self, patente_a_buscar):
-        if patente_a_buscar in self.patente:
+      def buscar_por_patente(self, texto_a_buscar):
+        if texto_a_buscar.upper() in self.nombre.upper() or texto_a_buscar.upper() in self.apellido.upper():
             return True
         else:
             return False
